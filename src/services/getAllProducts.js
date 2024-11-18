4d5a75b05da78fe60829eeee79e2a5da8dab2e62
faqhh <mnurfaqih7@gmail.com>
2024-11-18 17:58:49 +0700

    react js

---
+++ src/services/getAllProducts.js
@@ -0,0 +1,60 @@
+export default function getAllProducts() {
+  return [
+    {
+      id: 'NAF-1-07-FMEN-001',
+      slug: 'MASTER REM MODEL ER6 ',
+      name: "MASTER REM MODEL ER6 SET",
+      price: 3_50_000,
+      stock: 100,
+      imageUrl: '/assets/images/r6.jpg',
+      description: `MASTER REM ER6 UDAH SET KANAN KIRI  TERSEDIA 2 OPSi -HANDLE CHROME -HANDLE BLACK.`
+    },
+    {
+      id: 'NAF-1-07-FMEN-LV8-002',
+      slug: 'MASTER REM R25',
+      name: "HANDLE REM R25 ",
+      price: 2_50_000,
+      stock: 5,
+      imageUrl: `/assets/images/r25.jpg`,
+      description: `MASTER REM R25 / MT 25 ORIGINAL YAMAHA (BARANG LOST PACK)
+DENGAN DIAMETER MASTER REM 11 MM MEMILIKI TINGKAT PENGIRIMAN YANG LEBIH RESPONSIF DIBANDING MASTER REM YANG LAIN.
+WALAUPUN DIAMETER MASTER REM NYA 11MM TAPI TINGKAT PENGEREMAN YANG DIHASILKAN TIDAK KALAH DIBANDING MERK MASTER REM YANG LAIN.
+SUDAHDI UJI KUALITAS MATERIAL DI BALAP MAUPUN HARIAN.`
+    },
+    {
+      id: 'NAF-1-07-FWOMEN-003',
+      slug: 'nike-air-force-1-07-women',
+      name: "Velg Takasago Excel Asia Ring 18 RX KING Original Set Disetel",
+      price: 2_300_000,
+      stock: 0,
+      imageUrl: `/assets/images/tdr.jpg`,
+      description: `Velg Takasago Excel Asia Ring 18 RX KING Original Set Disetel
+velg original takasago excel asia
+tromol depan belakang original yamaha
+jari jari original yamaha
+barang dijamin original semua
+velg sudah disetel tinggal pasang`
+    },
+    {
+      id: 'NAF-1-07-FMEN-LV8-002',
+      slug: 'Velg Rossi VRossi Palang',
+      name: "Velg Rossi VRossi Palang ",
+      price: 1_500_000,
+      stock: 5,
+      imageUrl: `/assets/images/rossi.jpg`,
+      description: `V Rossi. pelek v rossi ring 14 palang 8. pelek v rossi ring 14 venom. pelek mio ring 14 variasi. velg racing mio sporty. velg nuovo ring 16.`
+    },
+    {
+      id: 'NAF-1-07-FMEN-LV8-002',
+      slug: 'VELG VND AK 55',
+      name: "VELG VND AK 55 ",
+      price: 1_500_000,
+      stock: 5,
+      imageUrl: `/assets/images/vnd.jpg`,
+      description: `VELG RACING VND AK 55 MODEL ENKEI 1.60X14 & 1.85X14 PLUG END PLAY KHUSUS HONDA MATIC; VARIO 125 VARIO 150
+BEAT SCOOPYVARIO 110
+TERSEDIA WARNA ORANGE HITAM MERAH BIRU GOLD.`
+    },
+
+  ]
+}
